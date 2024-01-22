9c73e067154bb1312c91bdb8392c0a6a4c9ceef6
wasfak <hardmodz@yahoo.com>
2024-01-22 18:38:12 +0200

    lasto

--- pages/api/hello.js
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import db from "@/db";
-
-export default function handler(req, res) {
-  db.connectDb();
-  res.status(200).json({ name: "John Doe" });
-  db.disconnectDb();
-}
